aa1566b50718b445d1dfbec48103653518b73a17
nicholaslu <nicholaslu1996@gmail.com>
2023-05-16 21:23:57 +0900

    Delete test Class

--- src/main/kotlin/NatNetClient.kt
+++ src/main/kotlin/NatNetClient.kt
@@ -1556,30 +1556,6 @@ class NatNetClient() {
         return offset
     }
 
-
-    class MulticastReceiver : Thread() {
-        protected var socket: MulticastSocket? = null
-        protected var buf = ByteArray(256)
-        override fun run() {
-            socket = MulticastSocket(4446)
-            val group = InetAddress.getByName("230.0.0.0")
-            socket!!.joinGroup(group)
-            while (true) {
-                val packet = DatagramPacket(buf, buf.size)
-                socket!!.receive(packet)
-                val received = String(
-                    packet.data, 0, packet.length
-                )
-                if ("end" == received) {
-                    break
-                }
-            }
-            socket!!.leaveGroup(group)
-            socket!!.close()
-        }
-    }
-
-
     private fun commandThreadFunction(inSocket: DatagramSocket, stop: () -> Boolean, gprintLevel: () -> Int): Int {
         val messageIdDict = mutableMapOf<String, Int>()
         if (!useMulticast) {
